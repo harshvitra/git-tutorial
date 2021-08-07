4c1ed038c7c67dda0ad1ec66f3a5854cad04ed22
Harsh Vitra <jainharsh44@gmail.com>
2021-08-07 15:30:31 +0530

    Changed font size

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className={styles.title} style={{fontSize:'20px'}}>
           Welcome to <a href="https://nextjs.org">Github!</a>
         </h1>
       </main>
